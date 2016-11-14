58c37f7f5a978c850061b22456b5ab432710caa0
Evgeny Shavlyugin <eshavlyugin@gmail.com>
2016-11-14 16:09:01 +0300

    option for read games from a predefined set

--- newalgo/game.cpp
+++ newalgo/game.cpp
@@ -4,14 +4,15 @@
 #include "generate.h"
 #include "player.h"
 #include "pymodels.h"
+#include "utils.h"
 
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
-void PlayLoop(const vector<std::shared_ptr<IPlayer>>& players, array<int, 3>& scores, bool dump, bool openCards, shared_ptr<ostream> recordFile) {
-	GameState state = GenLayout();
-	GameState stateCopy = GenLayout();
+void PlayLoop(const vector<std::shared_ptr<IPlayer>>& players, array<int, 3>& scores, bool dump, bool openCards, shared_ptr<ostream> recordFile, const GameState& layout) {
+	GameState state = layout;
+	GameState stateCopy = layout;
 	GameManager manager(players);
 	manager.SetNewLayout(state, openCards);
 	manager.SetDumpGames(dump);
@@ -54,8 +55,33 @@ void PlayGames(const po::variables_map& opt) {
 	    recordFileStream.reset(new ofstream(recordFile));
 	}
 	bool openCards = opt.count("open-cards");
-	for (int i = 0; i < numGames; i++) {
-		PlayLoop(players, scores, dump, openCards, recordFileStream);
+	string layoutsFile = opt["layouts-file"].as<string>();
+	vector<GameState> layouts;
+	if (layoutsFile.size() > 0) {
+		ifstream ist(layoutsFile);
+		string line;
+		while (std::getline(ist, line)) {
+			array<CardsSet, 3> hands;
+			for (uint32_t i = 0; i < 3; i++) {
+				for (const auto& str : utils::splitString<string>(line)) {
+					if (str.size() > 0) {
+						hands[i].Add(Card(str));
+					}
+				}
+				std::getline(ist, line);
+			}
+			uint32_t firstPlayer = std::stoi(line.c_str());
+			std::getline(ist, line);
+			std::getline(ist, line);
+			layouts.push_back(GameState(hands, firstPlayer, NoSuit));
+		}
+	} else {
+		for (uint32_t i = 0; i < numGames; i++) {
+			layouts.push_back(GenLayout());
+		}
+	}
+	for (const auto& layout : layouts) {
+		PlayLoop(players, scores, dump, openCards, recordFileStream, layout);
 		cerr << "Scores: " << scores[0] << ", " << scores[1] << ", " << scores[2] << endl;
 	}
 	cerr << "Simulation finished" << endl;
@@ -68,8 +94,8 @@ int main(int argc, char* argv[]) {
 	shared_ptr<IModelFactory> pyFactory(new PyModels::PyModelFactory());
 	auto nativeFactory = CreateNativeModelFactory();
 	auto compositeFactory = CreatePrefixModelFactory({make_pair(string("py"), pyFactory), make_pair(string("native"), nativeFactory)});
-	auto model = compositeFactory->CreateModel("py:model_lstm");
-	model->PredictSeq(vector<FeaturesSet>(8, CalcFeatures(GameState(), NoCard, 0, FT_PosPredict)));
+/*	auto model = compositeFactory->CreateModel("py:model_lstm");
+	model->PredictSeq(vector<FeaturesSet>(8, CalcFeatures(GameState(), NoCard, 0, FT_PosPredict)));*/
 	po::options_description desc("Allowed options");
 	const std::string formatDescr = "(format movePredictorsFolder:playingProbabilityPredFolder:trainingProbabilityPredFolder. In case we don't have prob folder for any of the components use random instead)";
 	desc.add_options()
@@ -80,6 +106,7 @@ int main(int argc, char* argv[]) {
 			("open-cards", "players are playing on open cards")
 			("num-games", po::value<int>()->default_value(1), "number of games to play")
 			("record-games-to-file", po::value<string>()->default_value(""), "Dump games to file")
+			("layouts-file", po::value<string>()->default_value(""), "Layouts to file")
 			;
 
 	po::variables_map vm;
